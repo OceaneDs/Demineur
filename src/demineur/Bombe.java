9abae03e2735e52dfe6504c64fe15e00554ad90d
bkasteller <43139731+bkasteller@users.noreply.github.com>
2018-11-03 18:11:22 +0100

    Bombe + grille + Case / fini = initialisation de la grille ok

--- src/demineur/Bombe.java
+++ src/demineur/Bombe.java
@@ -3,9 +3,11 @@ import java.lang.Math;
 
 public class Bombe
 {
-	private int nb_Bombe = 0, pourcentage = 50;
-	public int getPourcentage(){return this.pourcentage;} // Retourne le pourcentage de bombes
-	public void setPourcentage(int pourcentage){this.pourcentage = pourcentage;} // Modifie le pourcentage de bombes
-	public int getBombe(){return this.nb_Bombe;} // Retourne le nombre total de bombes
-	public boolean randBombe(){if( ((int) (Math.random() * 100)) > pourcentage) return false; this.nb_Bombe++; return true;} // Prend un nombre aléatoire entre 0 et 100, si ce nombre est supérieur aux pourcentage retourne false (n'est pas une bombe) sinon, incremente le nombre total de bombe avant et retourne true (est une bombe)
+	private int nb_Bombe = 0;
+	private int pBomb;
+	public Bombe(int pBomb){this.pBomb = pBomb;}
+	public int getPourcentage(){return this.pBomb;} // retourne le pourcentage de bombes
+	public void setPourcentage(int pourcentage){this.pBomb = pourcentage;} // Modifie le pourcentage de bombes
+	public int getBombe(){return this.nb_Bombe;} // retourne le nombre total de bombes
+	public boolean randBombe(){if( ((int) (Math.random() * 100)) > pBomb) return false; this.nb_Bombe++; return true;} // prend un nombre aleatoire entre 0 et 100, si ce nombre est superieur aux pourcentage retourne false (n'est pas une bombe) sinon, incremente le nombre total de bombe et retourne true (est une bombe)
 }
--- src/demineur/Grille.java
+++ src/demineur/Grille.java
@@ -1,19 +1,41 @@
 package demineur;
 
-public class Grille extends Bombe
+public class Grille
 {
-	private int taille = 0;
-	public Grille(int taille){this.taille = taille; iniGrille();} // définie la taille, puis appel l'initialisation de la grille
-	private void iniGrille() // créé la grille par défaut
+	private int taille = 10, pBomb = 50;
+	private Bombe bomb = new Bombe(pBomb);
+	private Case[][] grille = new Case[taille][taille]; // cree un tableau multidimetionnel de Case
+	public Grille(int taille, int pBomb){setTaille(taille); setPBomb(pBomb); iniGrille();} // definie la taille, puis appel l'initialisation de la grille
+	private void iniGrille() // cree la grille par defaut
 	{
-		Case[][] grille = new Case[taille][taille]; // créé un tableau multidimetionnel de Case
-		for(int x=0; x<taille; x++)
+		for(int x = 0; x < this.taille; x++)
 		{
-			for(int y=0; y<taille; y++)
+			for(int y = 0; y < this.taille; y++)
 			{
-				boolean bombe = randBombe();
-				grille[x][y] = new Case(bombe, false);
+				grille[x][y] = new Case(bomb.randBombe(), false);
 			}
 		}
 	}
+	public void afficher()
+	{
+		for(int x = 0; x < this.taille; x++)
+		{
+			for(int y = 0; y < this.taille; y++)
+			{
+				if(grille[x][y].getDiscovered() == false)
+					System.out.printf(" X |");
+				else 
+					System.out.printf("   |");
+			}
+			System.out.printf("\n");
+		}
+	}
+	public int getTaille(){return this.taille;} // retourne la taille de la grille
+	public void setTaille(int taille){this.taille = taille;} // modifie la taille de la grille
+	public int getPBomb(){return this.pBomb;} // retourne le pourcentage de bombes
+	public void setPBomb(int pBomb){this.pBomb = pBomb;} // modifie le pourcentage de bombes
 }
+
+
+//Grille grille = new Grille(10, 50);
+//grille.afficher();
